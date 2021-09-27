e089596a4b773cdd4976d8121942e194dc2b2da3
wawancallahan <wawan.callahan@gmail.com>
2021-09-27 13:45:57 +0800

    update text duta

--- penilaian_peserta_admin.php
+++ penilaian_peserta_admin.php
@@ -39,7 +39,7 @@ extract([
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard 2</title>
+    <title>Duta</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -83,7 +83,7 @@ extract([
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
                 <img src="assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">Duta</span>
             </a>
 
             <?php require './sidebar.php' ?>
--- tambah_kategori_peserta.php
+++ tambah_kategori_peserta.php
@@ -17,7 +17,7 @@ extract([
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard 2</title>
+    <title>Duta</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -61,7 +61,7 @@ extract([
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
                 <img src="assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">Duta</span>
             </a>
 
             <?php require './sidebar.php' ?>
